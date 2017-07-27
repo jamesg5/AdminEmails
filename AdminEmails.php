ad59020c3b1d83065ce1493ebf37319d69d5cb28
jamesg5 <james.geevarghese@nasa.gov>
2017-07-27 13:07:02 -0500

    Update AdminEmails.php

--- AdminEmails.php
+++ AdminEmails.php
@@ -32,7 +32,7 @@ class SpecialAdminEmails extends SpecialPage {
 
                 $bodyText="{|class='wikitable' \n!colspan='3' |[mailto:$allEmail Email All Admins] \n|- \n!User Name \n!Real Name \n!Email \n";
                 foreach( $res->result as $row ) {
-                        $bodyText .= " |- \n |[[User:$row[user_name]|$row[user_name]]] \n |{{#if:$row[user_email] | [[$row[user_real_name]]]| }} \n |{{#if:$row[user_email] | [mailto:$row[user_email] send email] | }} \n";
+                        $bodyText .= " |- \n |[[User:$row[user_name]|$row[user_name]]] \n |{{#if:$row[user_real_name] | [[$row[user_real_name]]]| }} \n |{{#if:$row[user_email] | [mailto:$row[user_email] send email] | }} \n";
                 }
                 $bodyText .= "|}";
                 $output->addWikiText($bodyText);
