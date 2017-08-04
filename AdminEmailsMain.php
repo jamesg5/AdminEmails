9d480382721351ddc7611a0d77160f0a12281a1f
jamesg5 <james.geevarghese@nasa.gov>
2017-08-04 15:31:59 -0500

    Update AdminEmailsMain.php

--- AdminEmailsMain.php
+++ AdminEmailsMain.php
@@ -31,7 +31,7 @@ class SpecialAdminEmails extends SpecialPage {
                         #var_dump($row);
                 }
 
-                $bodyText="{|class='wikitable' \n!colspan='3' |[mailto:$allEmail Email All Admins] \n!User Name \n!Real Name \n!Email \n";
+                $bodyText="{|class='wikitable' \n!colspan='3' |[mailto:$allEmail Email All Admins]|- \n!User Name \n!Real Name \n!Email \n";
                 foreach( $res->result as $row ) {
                         $bodyText .= " |- \n |[[User:$row[user_name]|$row[user_name]]] \n |{{#if:$row[user_real_name] | [[$row[user_real_name]]]| }} \n |{{#if:$row[user_email] | [mailto:$row[user_email] send email] | }} \n";
                 }
