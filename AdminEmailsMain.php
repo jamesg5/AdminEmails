380db5bda97f3116875bee6fa79aaeab066e3957
jamesg5 <james.geevarghese@nasa.gov>
2017-08-08 10:33:35 -0500

    Update AdminEmailsMain.php

--- AdminEmailsMain.php
+++ AdminEmailsMain.php
@@ -37,7 +37,6 @@ class SpecialAdminEmails extends SpecialPage {
                         $bodyText .= " |- \n |[[User:$row[user_name]|$row[user_name]]] \n |{{#if:$row[user_real_name] | [[$row[user_real_name]]]| }} \n |{{#if:$row[user_email] | [mailto:$row[user_email] send email] | }} \n";
                 }
                 $bodyText .= "|}";
-                $output = $bodyText;
                 $output->addPrimaryWikiText($bodyText);
         }
 }
