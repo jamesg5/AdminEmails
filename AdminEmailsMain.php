69ef07e59a791bc7c5a870f4a1766e04a5c0c85b
jamesg5 <james.geevarghese@nasa.gov>
2017-08-08 11:55:58 -0500

    Update AdminEmailsMain.php

--- AdminEmailsMain.php
+++ AdminEmailsMain.php
@@ -21,7 +21,8 @@ class SpecialAdminEmails extends SpecialPage {
                         array(),
                         array( 'user_groups' => array( 'JOIN', array( 'ug_user=user_id' )))
                 );
-
+		
+		$output->addHTML("__NOHEADER__\n__NOFOOTER__\n__NONSHEADER__\n__NONSFOOTER__");
                 global $wgSitename;
                 $output->addWikiText("'''Admin information for the " . $wgSitename . ":'''");
 
